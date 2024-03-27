cbf029329c9794239c271c16da5da2868def2abe
Pedro-Vhiana <pedrohb.0222@gmail.com>
2024-03-26 21:03:25 -0300

    Adicionando Constructors... usando sobrecarga e encapsulamento - 26.03

--- Entities/Product.java
+++ Entities/Product.java
@@ -2,9 +2,38 @@ package Entities;
 
 public class Product {
 	
-	public String name;
-	public double price;
-	public int quantity;
+	private String name;
+	private double price;
+	private int quantity;
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+														// Sobrecarga dos construtores.
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	
 	
 	public double totalValueInStock() {
 		double totalValueInStock = price * quantity;
--- Estudo/programStock.java
+++ Estudo/programStock.java
@@ -12,16 +12,16 @@ public class programStock {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		Product product = new Product(); // intânciando um objeto, esse objeto que será responsável pela comunicação dos arquivos.
-											
+		
 
 		System.out.println("Enter Product Data");
 		System.out.println("Product Name: ");
-		product.name = sc.nextLine(); // por conta do objeto criado podemos armazenar valores, chamando o objeto e atributo dentro da outra classe.
+		String name = sc.nextLine(); // por conta do objeto criado podemos armazenar valores, chamando o objeto e atributo dentro da outra classe.
 		System.out.println("Product Price: ");
-		product.price = sc.nextDouble();
-		System.out.println("Product quantity: ");
-		product.quantity = sc.nextInt();
+		double price = sc.nextDouble();
+		
+		Product product = new Product(name, price);
+		
 
 		System.out.println("");
 		System.out.println("Product Data: " + product);
@@ -30,6 +30,8 @@ public class programStock {
 		System.out.print("Enter the number of products to be added in stock:" );
 		int quantity = sc.nextInt(); // Criou uma variável para receber o valor
 		product.addProducts(quantity); // Chamou o método que adiciona no estoque, usando como parâmetro o valor passado na variável anterior.
+		
+		
 		
 		System.out.println("");
 		System.out.println("Update Data: " + product);
